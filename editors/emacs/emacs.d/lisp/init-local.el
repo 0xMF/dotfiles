cde53d468fbdb6df2f5c100e7f57cf7bf4175e98
Mark Fernandes <0xmf@outlook.com>
2017-02-25 02:18:53 -0500

    Make Source Code Pro Semibold my font in emacs too
    What change was done:
    
    	modified:   emacs/emacs.d/lisp/init-local.el
    
    Why was this change made:
    
      Hey that's the font I want.

--- editors/emacs/emacs.d/lisp/init-local.el
+++ editors/emacs/emacs.d/lisp/init-local.el
@@ -7,6 +7,14 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'dakrone t) ; other nice themes: 'grandshell
 
+;; Set default font -- copied from custom.el
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :weight semi-bold :height 90 :width normal)))))
+
 ;; keep purcell's emacs.d settings happy
 (provide 'init-local)
 
