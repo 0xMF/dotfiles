025893ea8c87eb18d40a8aa68230699a0820210b
Mark Fernandes <0xmf@outlook.com>
2017-02-25 09:53:39 -0500

    FIXES settings I am not interested in atm
    What change was done:
    
      new file:   editors/emacs/emacs.d/lisp/pre-load-local.el
    
    Why was this change made:
    
      Some of purcell's emacs settings I don't use or want, so mock provide
      for those files.
    
    Outstanding issues:

---
+++ editors/emacs/emacs.d/lisp/pre-load-local.el
@@ -0,0 +1,14 @@
+;;----------------------------------------------------------------------------
+;; bypass some purcell's emacs.d/init.el settings through provide stubs for
+;;    settings of languages and platforms I do not presently want of use.
+;;----------------------------------------------------------------------------
+
+(provide 'init-osx-keys)
+(provide 'init-textile)
+(provide 'init-erlang)
+(provide 'init-php)
+(provide 'init-nxml)
+(provide 'init-rails)
+(unless (version<= emacs-version "24.2")
+  (provide 'init-clojure)
+  (provide 'init-clojure-cider))
