3bb412c0abbc83bab626cc89622ac3e8d8642a8a
Mark Fernandes <0xMF@0xMF.com>
2019-05-13 16:47:19 -0400

    set SCP 9 in Emacs

--- editors/emacs/emacs.d/lisp/init-local.el
+++ editors/emacs/emacs.d/lisp/init-local.el
@@ -509,10 +509,10 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
 ;; optionally (set-frame-font "Source Code Pro Semibold-10")
 (cond
+ ((member "Source Code Pro" (font-family-list))
+  (set-frame-font "Source Code Pro-9:style=Semibold" nil t))
  ((member "Source Code Variable" (font-family-list))
   (set-frame-font "Source Code Variable-13:style=Semibold" nil t))
- ((member "Source Code Pro" (font-family-list))
-  (set-frame-font "Source Code Pro-13:style=Semibold" nil t))
  ((member "DejaVu Sans Mono" (font-family-list))
   (set-frame-font "DejaVu Sans Mono-10")))
 
