c70c8ea1d5ce0914b85784fb51d00646727433e5
Mark Fernandes <0xMF@0xMF.com>
2020-01-27 12:49:21 -0500

    move git based bash-aliases to functions

--- shells/bash/functions.bash
+++ shells/bash/functions.bash
@@ -513,6 +513,15 @@ function gcr {
   fi
 }
 
+# formerly aliases, now rewritten as functions
+gd    () { if _is_git_repo -eq 0; then git diff --color=always -w; fi }
+gdc   () { if _is_git_repo -eq 0; then git diff --color=always -w --cached; fi }
+gdh   () { if _is_git_repo -eq 0; then git diff --color=always -w HEAD; fi }
+gds   () { if _is_git_repo -eq 0; then git diff --color=always -w --staged; fi }
+gdiff () { if _is_git_repo -eq 0; then git diff --color=always -w HEAD; fi }
+gst   () { if _is_git_repo -eq 0; then git status ; fi }
+gsts  () { if _is_git_repo -eq 0; then git status -s; fi }
+
 function __gdh {
   if _is_git_repo -eq 0
   then
