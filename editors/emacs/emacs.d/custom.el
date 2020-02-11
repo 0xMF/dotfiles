7989c88c9f1dd52ea5766b2dcbdc0d16908af7eb
Mark Fernandes <0xMF@0xMF.com>
2020-02-11 17:32:39 -0500

    updates after tweaking colors

--- editors/emacs/emacs.d/custom.el
+++ editors/emacs/emacs.d/custom.el
@@ -14,7 +14,7 @@
  '(custom-enabled-themes (quote (smart-mode-line-powerline org-beautify)))
  '(custom-safe-themes
    (quote
-    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "119a9bc8d5a0051d7bdcfd72e6f4037c8207012086019806f1da491a926f9a4a" "eca86cec33b9da5f71eb23e8bba803f125420d76afa280c466f007ea7a4c2979" "c15efc857c82e8d3d1eb3c2570d78f0eaae347049c92b6dcb7b08b2f311644b0" "7c605bb846a2fd1252c86e50549294ccf71a1e81f58bc595731c321131a7e219" "c2d1ef5efc0a59769e33af6741083a77292536e39a3f485d5fdab61e57eb073b" "6a30a49339813df4b7a4fb51b219d3bcc36bdf54a5d823578c1e191e2be9b7e6" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "4639288d273cbd3dc880992e6032f9c817f17c4a91f00f3872009a099f5b3f84" default)))
+    ("eca86cec33b9da5f71eb23e8bba803f125420d76afa280c466f007ea7a4c2979" "10efa2432f01053505d36a08819b7b41aa34be5a521a4263f2d41b2209d00a28" "a71588763f11b430191769cb4460ef1e50a9ed895a6c1f2b53651410878c0340" "7c605bb846a2fd1252c86e50549294ccf71a1e81f58bc595731c321131a7e219" "fb4c2600dd2c174d4419f4c64149fae1459d9edee167e848bde02e0d6837a6b5" "6a30a49339813df4b7a4fb51b219d3bcc36bdf54a5d823578c1e191e2be9b7e6" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "b9e9ba5aeedcc5ba8be99f1cc9301f6679912910ff92fdf7980929c2fc83ab4d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "4639288d273cbd3dc880992e6032f9c817f17c4a91f00f3872009a099f5b3f84" default)))
  '(debug-on-error t)
  '(evil-shift-width 2)
  '(org-bullets-bullet-list (quote ("◉" "○" "✿" "✸")))
@@ -33,7 +33,7 @@
  '(org-block ((t (:foreground "Orange1" :background nil :box nil :weight bold))))
  '(org-block-begin-line ((t (:foreground "DarkOliveGreen" :background nil :weight semi-bold))))
  '(org-block-end-line ((t (:foreground "DarkOliveGreen" :background nil :weight semi-bold))))
- '(org-checkbox ((t (:background "#062451" :foreground "yellow" :box (:line-width -3 :color "#062451" :style "released-button")))))
+ '(org-checkbox ((t (:background "#180248" :foreground "yellow" :box (:line-width -3 :color "#180248" :style "released-button")))))
  '(org-code ((t (:foreground "Orange1" :box nil :weight bold))))
  '(org-document-title ((t (:foreground "#c8c8c8" :weight bold :height 1.2))))
  '(org-hide ((t (:foreground "#180248"))))
