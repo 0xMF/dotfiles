ef0fa52b8782bfcc3b03070c61d49c7e8a948a42
Mark Fernandes <0xMF@0xMF.com>
2021-02-09 20:36:36 -0500

    more streamling towards zsh

--- shells/zsh/zshrc.zsh
+++ shells/zsh/zshrc.zsh
@@ -2,7 +2,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="/home/mark/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
@@ -42,7 +42,7 @@ ZSH_THEME="robbyrussell"
 # DISABLE_AUTO_TITLE="true"
 
 # Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
+ ENABLE_CORRECTION="true"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
 # Caution: this setting can cause issues with multiline prompts (zsh 5.7.1 and newer seem to work)
@@ -70,11 +70,13 @@ ZSH_THEME="robbyrussell"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(archlinux colored-man-pages git perl systemadmin z)
+plugins=(archlinux colorize colored-man-pages git man perl systemadmin systemd z)
 
 source $ZSH/oh-my-zsh.sh
 
 # User configuration
+export ZSH_COLORIZE_TOOL=chroma
+export ZSH_COLORIZE_CHROMA_FORMATTER=terminal
 
 # export MANPATH="/usr/local/man:$MANPATH"
 
