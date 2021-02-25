411f10b52d496e15786792797fabcce4298947d9
Mark Fernandes <0xMF@0xMF.com>
2021-02-25 18:00:07 -0500

    a new serve

--- shells/all/pleasure.sh
+++ shells/all/pleasure.sh
@@ -5,5 +5,30 @@ alias pd='perldoc -MPod::Text::Color::Delight'
 alias perldoc='perldoc -MPod::Text::Color::Delight'
 
 alias tmux='tmux -u'
-alias http-serve="echo starting...port 221b is now \\\\0/; ruby -run -e httpd . -p 2212"
-alias serve="http-serve"
+alias http-serve="serve"
+serve() {
+
+  local port=2212
+  dirs=`dirs | tr ' ' '\n' | wc -l`
+  if [[ -d "$1" ]]; then
+    if [[ -e /usr/bin/ruby ]]; then
+      serverstr="/usr/bin/ruby -run -e httpd $1 -p $port"
+    else
+      pushd $1
+      serverstr="/usr/bin/python -m http.server $port"
+    fi
+  else
+    if [[ -e /usr/bin/ruby ]]; then
+      serverstr="/usr/bin/ruby -run -e httpd . -p $port"
+    else
+      serverstr="/usr/bin/python -m http.server $port"
+    fi
+  fi
+
+    echo "Starting: $serverstr on `ip -br -4 a|sed '/^lo/d'|cut -d' ' -f1,14- | tr -s ' ' | sed 's/ / at /'`"
+
+    eval $serverstr
+
+    d=`dirs | tr ' ' '\n' | wc -l`
+    [[ $d -ne $dirs ]] && popd
+}
