afb4533de95c66f3b5aaacafee2e4886f8ac9f1c
Mark Fernandes <0xMF@0xMF.com>
2021-03-01 23:59:46 -0500

    update to ksh function notation

--- shells/bash/functions.bash
+++ shells/bash/functions.bash
@@ -6,29 +6,29 @@
 
 OS=$(uname -s)
 
-showdoc() {
+function showdoc {
 
   [[ ! -s "$1" ]] && { 2>&1 echo "Usage: showdoc FILE from dotfiles/doc"; return 1; }
   which -a vimpager > /dev/null 2>&1
   [[ $? -eq 0 ]] && vimpager $1 || cat $1
 }
 
-ll() {
+function ll {
 
   [[ -z "$1" ]]\
     && ls -lhFt --time-style=+"%Y-%b-%d %H:%S" | sed "/^total /d"\
     || ls -lhFt --time-style=+"%Y-%b-%d %H:%S" "$@" | sed "/^total /d"
 }
 
-ls-files-only() {
+function ls-files-only {
   eval '[[ $# -eq 0 ]] && \ls -dF * || { for d in "$@"; do \ls -dF ${d}/*; done } | sed "\|/$|d"'
 }
 
-ls-all-files-only() {
+function ls-all-files-only {
   find "$@" -maxdepth 1 -type f
 }
 
-record() {
+function record {
   if [[ `type -t termtosvg` == "file" ]]
   then
     termtosvg -c '/bin/bash --init-file ~/.bash/minimal.bash' \
@@ -36,11 +36,11 @@ record() {
   fi
 }
 
-lsc() {
+function lsc {
   ls "$@" | cut -c1-44 | column -c "${COLUMNS:-88}"
 }
 
-xfci() {
+function xfci {
   git update-index --assume-unchanged $HOME/repos/dotfiles/misc/config/xfce4/terminal/terminalrc
   pushd $HOME/repos/dotfiles/misc/config/xfce4/xfconf/xfce-perchannel-xml  > /dev/null
   for f in `echo *`
@@ -50,7 +50,7 @@ xfci() {
   popd > /dev/null
 }
 
-xfcni() {
+function xfcni {
   git update-index --no-assume-unchanged $HOME/repos/dotfiles/misc/config/xfce4/terminal/terminalrc
   pushd $HOME/repos/dotfiles/misc/config/xfce4/xfconf/xfce-perchannel-xml  > /dev/null
   for f in `echo *`
@@ -61,7 +61,7 @@ xfcni() {
 }
 
 # show directory tree
-trd() {
+function trd {
     depth="$1"
     level="-L 1"
     path="$2"
@@ -80,7 +80,7 @@ trd() {
     eval "tree $level $path $@"
 }
 
-poof() {
+function poof {
   sudo sync
   sudo sync
   sudo sync
@@ -88,7 +88,7 @@ poof() {
 }
 
 # Environment variable settings for setting DISPLAY to local or remote
-get_xserver() {
+function get_xserver {
   case $TERM in
     xterm )
         XSERVER=$(who am i | awk '{print $NF}' | tr -d ')''(' )
@@ -99,7 +99,7 @@ get_xserver() {
   esac
 }
 
-set_display_env() {
+function set_display_env {
   if [ -z ${DISPLAY:=""} ]; then
     get_xserver
     if [[ -z ${XSERVER} || ${XSERVER} == $(hostname) || ${XSERVER} == "unix" ]]; then
@@ -111,7 +111,7 @@ set_display_env() {
   export DISPLAY
 }
 
-share() {
+function share {
   mkdir -p $HOME/share
   sudo mount -t vboxsf -o uid=$UID,gid=$(id -g),dmode=700,fmode=600,umask=077 share $HOME/share
   [ $? -eq 0 ] \
@@ -122,23 +122,23 @@ share() {
       )
 }
 
-su() {
+function su {
   SU=$(which su)
   [ -z "$1" ] && $SU -m || $SU "$@"
 }
 
-anc() {
+function anc {
   alias ls='lsn'
   alias tree='tree -n'
   alias grep='grep --color=none -i'
 }
 
-beep() {
+function beep {
   paplay /usr/share/sounds/freedesktop/stereo/service-login.oga
   paplay /usr/share/sounds/freedesktop/stereo/service-logout.oga
 }
 
-alarm() {
+function alarm {
   if [ ! -z "$1" ]
   then
     echo "alarm will go off in $1 minutes"
@@ -169,19 +169,19 @@ function g+++ {
   fi
 }
 
-loc() {
+function loc {
   `which localc` "$@" 2>/dev/null &
 }
 
-low() {
+function low {
   `which lowriter` "$@" 2>/dev/null &
 }
 
-erls() {
+function erls {
   `which erl` "$@" -pa ebin -pa deps/*/ebin
 }
 
-emacs() {
+function emacs {
   EMACS=$(which -a emacs|sed '1q')
   if [ -z "${EMACS}" ]
   then
@@ -197,16 +197,16 @@ emacs() {
   fi
 }
 
-ew() {
+function ew {
   `which -a emacs-works|sed '1q'` "$@" 2>/dev/null && [ `which pal 2>/dev/null` ] && pal &
 }
 
-end() {
+function end {
   #`which -a emacs|sed '1q'` "$@" --no-desktop 2>/dev/null &
   emacs "$@" --no-desktop &
 }
 
-otp() {
+function otp {
   if [ -d $HOME/repos/otp ];
   then
     pushd $HOME/repos/otp > /dev/null
@@ -225,7 +225,7 @@ otp() {
   fi
 }
 
-sl() {
+function sl {
   SL=$(which -a sl|sed '1q')
   echo ${SL}
   if [ -n "${SL}" ]
@@ -236,7 +236,7 @@ sl() {
 
 # filter markdown file given through pandoc and read in w3m
 # else locate [rRiI] (readme/index) markdown files in given path or use pwd
-lessmd() {
+function lessmd {
 
   mdfiles=[rRiI]*.md
   path=${1:-.}
@@ -248,14 +248,14 @@ lessmd() {
               || pandoc `echo "$files"` | w3m -num -s -T text/html
 }
 
-ds() {
+function ds {
   h=$(df -hTxtmpfs -xdevtmpfs | head -1)
   b=$(df -hTxtmpfs -xdevtmpfs | sed -n '2,$p'| sort -hk6)
   printf "%s\n%s\n" "$h" "$b"
   dpigs -H
 }
 
-recent() {
+function recent {
   OLDPWD=$(pwd)
   cd $HOME
   find ~ -atime 0 -ctime 0 -mtime 0 -type f |\
@@ -267,7 +267,7 @@ recent() {
   cd $OLDPWD
 }
 
-mouse-reset() {
+function mouse-reset {
   sudo modprobe -r psmouse
   sleep 1
   sudo modprobe psmouse
