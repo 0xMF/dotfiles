9ad7b89796f39e32d8704e118408b77a70516ae9
Mark Fernandes <0xMF@0xMF.com>
2021-03-06 10:14:19 -0500

    cleanup

--- shells/0xMF/prompt.sh
+++ shells/0xMF/prompt.sh
@@ -1,30 +1,4 @@
 echo -n loading prompts...
-shell=`ps o command -p $$ | grep -v "^COMMAND$" | tr -d '-' | cut -d' ' -f1`
-export SHELL_PROMPT=$( [ "$shell" = "bash" ] && echo $ || echo %)
-
-if [ "$shell" = "zsh" ]; then
-  BLACK="%{$fg[black]%}"
-  GREY="%{$fg_bold[grey]%}"
-  RED="%{$fg_bold[red]%}"
-  GREEN="%{$fg_bold[green]%}"
-  YELLOW="%{$fg_bold[yellow]%}"
-  BLUE="%{$fg_bold[blue]%}"
-  MAGENTA="%{$fg_bold[magenta]%}"
-  CYAN="%{$fg_bold[cyan]%}"
-  WHITE="%{$fg_bold[white]%}"
-  NOCOLOR="%{$reset_color%}"
-else
-  BLACK="\[\033[30m\]"
-  GREY="\[\033[1;30m\]"
-  RED="\[\033[1;31m\]"
-  GREEN="\[\033[1;32m\]"
-  YELLOW="\[\033[1;33m\]"
-  BLUE="\[\033[1;34m\]"
-  PURPLE="\[\033[1;35m"
-  CYAN="\[\033[1;36m\]"
-  WHITE="\[\033[1;37m\]"
-  NOCOLOR="\[\033[00m\]"
-fi
 
 function psdark {
   # blue dir, green link, light green executables
